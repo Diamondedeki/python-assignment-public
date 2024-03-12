661e7814d545f181c8545772dc49f480d4e7e937
Divine Edeki <118979779+Diamondedeki@users.noreply.github.com>
2024-03-12 12:24:31 -0700

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,113 @@
+# Exercises: Level 1
+
+# 1. Declare a first name variable and assign a value to it
+firstname = 'Divine'
+print(firstname)
+
+# 2.Declare a last name variable and assign a value to it
+lastname = 'Edeki'
+print(lastname)
+
+# 3. Declare a full name variable assign a value to it
+fullname = 'Divine Edeki'
+print(fullname)
+
+# 4. Declare a country variable and assign a value to it
+country = 'Nigeria'
+print(country)
+
+# 5. Declare a city variable and assign a value to it
+city = 'Jos'
+print(city)
+
+# 6. Declare an age variable and assign a value to it
+age = 14
+print(age)
+
+# 7. Declare a year variable and assign a value to it
+year = 2024
+print(year)
+
+# 8. Declare a variable is_student and assign a value to it
+is_student = True
+print(is_student)
+
+# 9. Declare a variable is_true and assign a value to it
+is_true = True
+print(is_true)
+
+# 10. Declare a variable is_light_on and assign a value to it
+is_light_on = 'Yes'
+print(is_light_on)
+
+# 11. Declare multiple variable on one line
+name, age, country, is_student= 'Divine', 14, 'Nigeria', True
+print(name, age, country, is_student)
+# Exercises: Level 2
+
+# 1. Check the data type of all your variables using type() built-in function
+print(type(firstname))
+print(type(lastname))
+print(type(fullname))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_student))
+print(type(is_true))
+print(type(is_light_on))
+
+# 2. Using the len() built-in function, find the length of your first name
+print(len(firstname))
+
+# 3. Compare the length of your first name and your last name
+print(len(lastname))
+
+# 4. Declare 5 as num_one and 4 as num_two
+num_one = 5
+print(num_one)
+num_two = 4
+print(num_two)
+
+# ○ Add num_one and num_two and assign the value to a variable total
+total = num_one + num_two
+print(total)
+
+# ○ Subtract num_two from num_one and assign the value to a variable difference 
+difference = num_one - num_two
+print(difference)
+
+# ○ Multiply num_two and num_one and assign the value to a variable product
+product = num_two * num_one
+print(product)
+
+# ○ Divide num_one by num_two and assign the value to a variable division
+division = num_one / num_two
+print(division)
+
+# ○ Use modulus division to find num_two divided by num_one and assign the value to a variable remainder
+remainder = num_one % num_two
+print(remainder)
+
+# ○ Calculate num_one to the power of num_two and assign the value to a variable exp
+exp = num_one **num_two
+print(exp)
+
+# ○ Find floor division of num_one by num_two and assign the value to a variable floor_division
+floor_division = num_one // num_two
+print(floor_division)
+
+# 5. The radius of a circle is 30 meters.
+# ○ Calculate the area of a circle and assign the value to a variable name of area_of_circle
+are_of_circle = 2826
+print(are_of_circle)
+
+# ○ Calculate the circumference of a circle and assign the value to a variable name of circum_of_circle
+circum_of_circle = 188.4
+print(circum_of_circle)
+
+# 	6. Use the built-in input function to get first name, last name, country and age from a user and store the value to their corresponding variable names
+print(input(firstname))
+print(input(lastname))
+print(input(country))
+print(input(age))
